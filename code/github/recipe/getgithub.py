8f019ea65ede634de40ca31afbb0ed3d7ca0fb13
iammachine <iammachine@users.noreply.github.com>
2015-06-27 02:06:03 +0530

    initial getgithub.py commit.
    Python script to uses the "GitHub API" to scrape public repository data.
    https://developer.github.com/v3/

---
+++ code/github/recipe/getgithub.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# @author     : Prabhat Kumar
+# @copyright  : © 2015, Prabhat Kumar
+# ===================================
+__author__ = 'Prabhat Kumar'
+# Python script to uses the "GitHub API" to scrape public repository data.
+# https://developer.github.com/v3/
+# ===================================
